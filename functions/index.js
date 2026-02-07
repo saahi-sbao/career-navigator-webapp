6f6ade2750128c4c15a5234bf5f46076725938eb
Sadiq <sadiq14526@gmail.com>
2026-02-07 10:03:24 +0000

    FirebaseError: Missing or insufficient permissions: The following reques

--- functions/index.js
+++ functions/index.js
@@ -21,7 +21,7 @@ const logger = require("firebase-functions/logger");
 // functions should each use functions.runWith({ maxInstances: 10 }) instead.
 // In the v1 API, each function can only serve one request per container, so
 // this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10, region: "us-central1" });
+setGlobalOptions({ maxInstances: 10 });
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
